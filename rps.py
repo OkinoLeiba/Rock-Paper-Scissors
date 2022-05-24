90f750e22947a1fe9eb710c2c18ec69f6ac71785
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-05-24 17:10:50 -0400

    rsp.py
    Improved program: function func_rand will generate random value each time the program iterators, added conditional statement to exit program, added static variables to class, and added constructor to class.

--- rps.py
+++ rps.py
@@ -10,72 +10,95 @@ paper = "paper"
 scissors = "scissors"
 global rps 
 rps = ("rock", "paper", "scissors")
-rps_dic = {1: "rock", 2: "paper", 3: "scissors"}
+#rps_dic = {1: "rock", 2: "paper", 3: "scissors"}
+end_program = "Sho'Nuff is the Master"
 
 
-def func_rand():
-    #randomize the values in tuple
-    ran_rps = random.choice(rps)  
-    return ran_rps
-    pass
 
 
 class class_rps: 
 
+    #unnecessary but defining static data members/variables in class good stucture
+    rock = "rock"
+    paper = "paper"
+    scissors = "scissors"
+    rps = ("rock", "paper", "scissors")
+    #rps_dic = {1: "rock", 2: "paper", 3: "scissors"}
+    end_program = "Sho'Nuff is the Master"
+
+
+     #unnecessary but defining constructor in class good stucture
+    def __init__(self, rock, paper, scissors, rps):
+        return super().__init__(rock, paper, scissors, rps)
+        
+    def func_rand():
+        #randomize the values in tuple
+        rand_rps = random.choice(class_rps.rps) 
+        return rand_rps
+        pass
+
     #Conditional parameter for function rock 
-    def func_rock():  
-        if ran_rps == rock:
-            print("Draw, try again.")
-        elif ran_rps == paper:
-            print("Paper covers rock, you win.")
-        elif ran_rps == scissors:
-            print("Rock breaks scissors, I win.")
+    def func_rock(rand_rps):
+        print(rand_rps.title())
+        if rand_rps == class_rps.rock:
+            print("Draw, try again.\n")
+        elif rand_rps == paper:
+            print("Paper covers rock, I win.\n")
+        elif rand_rps == scissors:
+            print("Rock breaks scissors, you win.\n")
         else:
-            raise Exception("The Gods are Angry")
+            raise Exception("The Gods are Angry.\n")
         pass
 
 
     #Conditional parameter for function paper 
-    def func_paper():  
-        if ran_rps == paper:
-            print("Draw, try again.")
-        elif ran_rps == scissors:
-            print ("Scissors cuts paper, I win.")
-        elif ran_rps == rock:
-            print("Paper covers rock, you win.") #redun
+    def func_paper(rand_rps): 
+        print(rand_rps.title())
+        if rand_rps == class_rps.paper:
+            print("Draw, try again.\n")
+        elif rand_rps == scissors:
+            print ("Scissors cuts paper, I win.\n")
+        elif rand_rps == rock:
+            print("Paper covers rock, you win.\n") #redun
         else:
-            raise Exception("The Gods are Angry")
+            raise Exception("The Gods are Angry.\n")
         pass
 
     #Conditional parameter for function paper
-    def func_scissors():
-        if ran_rps == scissors:
-            print("Draw, try again.")
-        elif ran_rps == rock:
-            print("Rock breaks scissors, I win.") #redun
-        elif ran_rps == paper:
-            print("Scissors cuts paper. you win.") #redun
+    def func_scissors(rand_rps):
+        print(rand_rps.title())
+        if rand_rps == class_rps.scissors:
+            print("Draw, try again.\n")
+        elif rand_rps == rock:
+            print("Rock breaks scissors, I win.\n") #redun
+        elif rand_rps == paper:
+            print("Scissors cuts paper, you win.\n") #redun
         else:
-            raise Exception("The Gods are Angry")
+            raise Exception("The Gods are Angry.\n")
         pass
 
 
 
     
 f_rps = 'fin'
-while f_rps != rps:
+while f_rps != class_rps.rps:
     #User Input
     u_rps = input("Rock, Paper, Scissors say shoot: ")
     l_rps = u_rps.lower()
-    func_rand()
-    if l_rps == rock:
-        class_rps.func_rock()
-    elif l_rps == paper:
-        class_rps.func_paper()
-    elif l_rps == scissors:
-        class_rps.func_scissors()
-    else:
-        exit
+    rand_rps = class_rps.func_rand()
+    if l_rps == class_rps.rock:
+        class_rps.func_rock(rand_rps)
+    elif l_rps == class_rps.paper:
+        class_rps.func_paper(rand_rps)
+    elif l_rps == class_rps.scissors:
+        class_rps.func_scissors(rand_rps)
+    elif u_rps != class_rps.rps:
+        print("Please type rock, paper, or scissors")
+        print("To quit type \"Sho'Nuff is the Master\"\n")
+    elif u_rps == class_rps.end_program:
+        sys.exit()
+    else:   
+        raise Exception("The Gods are Angry.\n")
     
 
 
